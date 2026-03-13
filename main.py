8d64facb8d1db4cbad9c3d0436f6e1482133bb2e
ayik kuni zahroka <bintangazra279@gmail.com>
2026-03-12 21:07:32 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from book import Book
+import datetime
 
 buku1 = Book(1, "Belajar Python", "Affan", 2025)
+buku2 = Book(2, "belajar java", "Budi", 2024)
 
 print(buku1.judul)
+print(buku1.get_update_terakhir())
+
+buku1.update_judul("mulai belajar phyton bab 1 😉")
+
+print(buku1,judul)
+print(buku1.get_update_terakhir())
